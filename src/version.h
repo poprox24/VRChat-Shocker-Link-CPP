0c603c2d1ee97635d1d3dc0b0f5f313897f6c9a7
poprox24 <hudec.matyas.2006@gmail.com>
2026-03-20 22:56:38 +0100

    Forgot to update version

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 // clang-format off
 #pragma once
 
-#define APP_VERSION "1.2.1"
+#define APP_VERSION "1.2.2"
 #define APP_RELEASE_NAME "Settings UI to replace config.yml, all settings now in settings.json"
