8a1f71b7e95d84db2dd88b50b0e67ff1cc04c8b3
poprox24 <hudec.matyas.2006@gmail.com>
2026-03-24 17:52:27 +0100

    Forgot to update version

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // clang-format off
 #pragma once
 
-#define APP_VERSION "1.3.3"
+#define APP_VERSION "1.3.4"
 #define APP_RELEASE_NAME "Added panic hotkey (can be changed in settings), Undo/Redo Support"
 #define APP_RELEASE_NOTES "Patch:\n - Added panic hotkey (can be changed in settings)\n - New accent color setting\n - Changed default app colors\n - CTRL+Z/CTRL+Y support on all values in UI"
