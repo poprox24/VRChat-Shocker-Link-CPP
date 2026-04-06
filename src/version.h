64aff3cc18d69121441e5f7becfc95b98ace22e1
poprox24 <hudec.matyas.2006@gmail.com>
2026-04-06 21:06:15 +0200

    Bump version

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // clang-format off
 #pragma once
 
-#define APP_VERSION "1.4.5"
-#define APP_RELEASE_NAME "Added support for PiShock and OpenShock API + new stats menu"
-#define APP_RELEASE_NOTES "Patch:\n - Added support for PiShock/OpenShock API\n - If no shockers are set automatically get them from the API\n - Added stats menu"
+#define APP_VERSION "1.5.0"
+#define APP_RELEASE_NAME "Linux compatiblity"
+#define APP_RELEASE_NOTES "APIs and hotkey doesn't work yet"
