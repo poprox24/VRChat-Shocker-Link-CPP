4572bdd32397e4324f3f32e54fe7dc166751c077
poprox24 <hudec.matyas.2006@gmail.com>
2026-04-09 01:06:49 +0200

    Bump version

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // clang-format off
 #pragma once
 
-#define APP_VERSION "1.5.2"
+#define APP_VERSION "1.5.3"
 #define APP_RELEASE_NAME "Linux compatiblity"
 #define APP_RELEASE_NOTES "Patch:\n - Fully compatible with linux\n - Switched from DX11 to OpenGL\n - Global hotkey doesn't work on Windows, keep the window focused to use."
