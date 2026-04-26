0971e3d5b33d59b86d006bda348dae4b399155a1
poprox <hudec.matyas.2006@gmail.com>
2026-04-26 17:41:41 +0200

    Bump version

--- src/version.h
+++ src/version.h
@@ -3,4 +3,4 @@
 
 #define APP_VERSION "1.7.0"
 #define APP_RELEASE_NAME "Added visual parameters via OSC"
-#define APP_RELEASE_NOTES "Patch:\n - Added visual parameters via OSC"
+#define APP_RELEASE_NOTES "Patch:\n - Added visual parameters via OSC\n - Fixed program icon not showing up\n - Fixed symbols not rendering on Windows"
