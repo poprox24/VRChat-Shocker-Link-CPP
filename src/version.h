a2ac4748f63be7647bf2b9ca87de80efd9040a25
poprox <hudec.matyas.2006@gmail.com>
2026-04-29 03:50:25 +0200

    Bump version

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // clang-format off
 #pragma once
 
-#define APP_VERSION "1.7.1"
-#define APP_RELEASE_NAME "Added visual parameters via OSC"
-#define APP_RELEASE_NOTES "Patch:\n - Added visual parameters via OSC\n - Fixed program icon not showing up\n - Fixed symbols not rendering on Windows"
+#define APP_VERSION "1.7.2"
+#define APP_RELEASE_NAME "Modernize UI"
+#define APP_RELEASE_NOTES "Patch:\n - Modernize UI"
