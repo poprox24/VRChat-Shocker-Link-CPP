f787290f87ef62f440ef7aa73165518791760b46
poprox <hudec.matyas.2006@gmail.com>
2026-05-09 01:16:43 +0200

    Bump version

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // clang-format off
 #pragma once
 
-#define APP_VERSION "1.7.2"
+#define APP_VERSION "1.7.3"
 #define APP_RELEASE_NAME "Modernize UI"
 #define APP_RELEASE_NOTES "Patch:\n - Modernize UI"
